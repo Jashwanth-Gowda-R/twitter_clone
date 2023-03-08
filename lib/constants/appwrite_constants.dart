1152ca7887d23296f415e35cf8680e7a2ae59537
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-03-08 16:30:17 +0530

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants{
+  static const String databaseId='640862deae5e1c32d852';
+  static const String projectId = '64085dbccd808897bd1e';
+  static const String endpoint='http://localhost:80/v1';
+}
