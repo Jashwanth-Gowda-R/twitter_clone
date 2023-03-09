c6beeacde07c87a81aa6fa820feb59c36661bbc3
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-03-09 22:21:21 +0530

    added appwrite constants with formatting

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,5 +1,5 @@
-class AppwriteConstants{
-  static const String databaseId='640a0bde80bdca86186b';
+class AppwriteConstants {
+  static const String databaseId = '640a0bde80bdca86186b';
   static const String projectId = '640a0adadbfeb4d86afa';
-  static const String endpoint='http://localhost:80/v1';
+  static const String endpoint = 'http://localhost:80/v1';
 }
