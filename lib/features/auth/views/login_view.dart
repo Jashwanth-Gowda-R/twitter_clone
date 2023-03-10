373839b24f7b94d41d8a89aa77c990da0efd2ce9
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-03-10 18:40:36 +0530

    added textfield for auth screens

--- lib/features/auth/views/login_view.dart
+++ lib/features/auth/views/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/constants/ui_constants.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,8 +10,48 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  // to avoid rebuilding
+  final appBar = UIConstants.appBar();
+  // text fields
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose.
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: appBar,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              children: [
+                AuthField(
+                  controller: _email,
+                  hint: 'Email',
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                AuthField(
+                  controller: _password,
+                  hint: 'Password',
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
