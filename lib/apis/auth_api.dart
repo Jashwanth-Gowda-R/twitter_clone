36893ff2ad1930d508af2fcab7c52a0e968508ae
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-03-11 18:23:47 +0530

    Signing Up User with Appwrite Auth

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as models;
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/failure.dart';
+
+import 'package:twitter_clone/core/type_defs.dart';
+
+abstract class IAuthAPI {
+  FutureEither<models.Account> signUp({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+  AuthAPI({
+    required Account account,
+  }) : _account = account;
+
+  @override
+  FutureEither<models.Account> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final account = await _account.create(
+        userId: 'unique()',
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } catch (e, stackTrace) {
+      return left(
+        Failure(
+          e.toString(),
+          stackTrace,
+        ),
+      );
+    }
+  }
+}
