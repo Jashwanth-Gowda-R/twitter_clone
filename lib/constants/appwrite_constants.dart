a341ac5bb886c7acde9bf168cd35c4cca4938fd7
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-03-14 16:12:48 +0530

    Solving Appwrite unauthorized_scope Problem

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,6 +1,6 @@
 class AppwriteConstants {
   static const String databaseId = '640a0bde80bdca86186b';
   static const String projectId = '640a0adadbfeb4d86afa';
-  static const String endpoint = 'http://localhost:80/v1';
+  static const String endpoint = 'http://192.168.0.105:80/v1';
   static const String userCollectionId = '64102d9d8ec92eaa31c8';
 }
