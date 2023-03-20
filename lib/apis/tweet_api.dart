96211d72dc5882d76287ab53db591f5f225b639c
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-03-20 20:40:18 +0530

    Storing Tweets to Appwrite Database & Appwrite Storage

---
+++ lib/apis/tweet_api.dart
@@ -0,0 +1,52 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/core/failure.dart';
+import 'package:twitter_clone/core/providers.dart';
+import 'package:twitter_clone/core/type_defs.dart';
+import 'package:twitter_clone/models/tweet_model.dart';
+
+final tweetAPIProvider = Provider((ref) {
+  final db = ref.watch(appwriteDatabaseProvider);
+  return TweetAPI(
+    db: db,
+  );
+});
+
+abstract class ITweetAPI {
+  FutureEither<Document> shareTweet(Tweet tweet);
+}
+
+class TweetAPI implements ITweetAPI {
+  final Databases _db;
+  // final Realtime _realtime;
+  TweetAPI({
+    required Databases db,
+    // required Realtime realtime,
+  }) : _db = db;
+  // _realtime = realtime;
+
+  @override
+  FutureEither<Document> shareTweet(Tweet tweet) async {
+    try {
+      final document = await _db.createDocument(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.tweetsCollection,
+        documentId: ID.unique(),
+        data: tweet.toMap(),
+      );
+      return right(document);
+    } on AppwriteException catch (e, st) {
+      return left(
+        Failure(
+          e.message ?? 'Some unexpected error occurred',
+          st,
+        ),
+      );
+    } catch (e, st) {
+      return left(Failure(e.toString(), st));
+    }
+  }
+}
--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,6 +1,15 @@
 class AppwriteConstants {
   static const String databaseId = '640a0bde80bdca86186b';
   static const String projectId = '640a0adadbfeb4d86afa';
-  static const String endpoint = 'http://192.168.0.105:80/v1';
+  static const String endpoint = 'http://192.168.0.106:80/v1';
+  // static const String endpoint = 'http://localhost:80/v1';
   static const String userCollectionId = '64102d9d8ec92eaa31c8';
+
+  static const String tweetsCollection = '6418669b2f3d31261de9';
+  // static const String notificationsCollection = '63cd5ff88b08e40a11bc';
+
+  static const String imagesBucket = '6418745be5983496ed4d';
+
+  static String imageUrl(String imageId) =>
+      '$endpoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
 }
